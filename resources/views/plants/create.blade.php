5bbd54a19b1e2225f9facb04ce4a2fd33bc34953
Tommaso-Panarotto <tommaso.panarotto@gmail.com>
2024-10-29 16:39:33 +0100

    page create form

---
+++ resources/views/plants/create.blade.php
@@ -0,0 +1,41 @@
+@extends("layouts.app")
+
+@section("head-title", "add a new plant")
+
+@section("main-content")
+<div class="container text-center">
+  <h1 class="mt-5">Aggiungi una nuova pianta</h1>
+  <form class="mt-5" method="POST" action="{{route("plant.store")}}">
+    @csrf
+
+    <div class="mb-3">
+      <label for="name" class="form-label">nome pianta:</label>
+      <input type="text" class="form-control" id="plant-name" name="name">
+    </div>
+    <div class="mb-3">
+        <label for="species" class="form-label">specie pianta:</label>
+        <input type="text" class="form-control" id="plant-species" name="species">
+      </div>
+      <div class="mb-3">
+        <label for="habitat" class="form-label">habitat pianta:</label>
+        <input type="text" class="form-control" id="plant-habitat" name="habitat">
+      </div>
+      <div class="mb-3">
+        <label for="exposure" class="form-label">esposizione pianta:</label>
+        <input type="text" class="form-control" id="plant-exposure" name="exposure">
+      </div>
+      <div class="mb-3">
+        <label for="description" class="form-label">descrizione pianta:</label>
+        <input type="text" class="form-control" id="plant-description" name="description">
+      </div>
+      <div class="mb-3">
+        <label for="image" class="form-label">immagine pianta:</label>
+        <input type="text" class="form-control" id="plant-image" name="image">
+      </div>
+      <div class="buttons">
+          <button type="submit" class="btn btn-primary">Invia</button>
+          <button type="reset" class="btn btn-warning">Reset campi</button>
+      </div>
+  </form>
+</div>
+@endsection
