6a3aabb33a5c0f68b1705098c4071a9872d149a8
Tommaso-Panarotto <tommaso.panarotto@gmail.com>
2024-10-29 16:05:57 +0100

    style show page

---
+++ resources/views/plants/show.blade.php
@@ -0,0 +1,24 @@
+@extends("layouts.app")
+
+@section("head-title", "Plants")
+
+@section("main-content")
+<div class="container text-center">
+  <h2>{{$plant["name"]}}</h2>
+  <div class="row mt-5 justify-content-center">
+    <div class="col-4">
+      <div class="card">
+        <img src="{{$plant["image"]}}" alt="{{$plant["name"]}}">
+        <div class="card-body">
+          <ul class="text-start">
+            <li><b>specie:</b> {{$plant["species"]}}</li>
+            <li><b>habitat:</b> {{$plant["habitat"]}}</li>
+            <li><b>esposizione:</b> {{$plant["exposure"]}}</li>
+            <li><b>descrizione:</b> {{$plant["description"]}}</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+@endsection
