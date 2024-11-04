f98ff6cedb9c68ada71f61f1ae91d2325c6fee0c
Tommaso-Panarotto <tommaso.panarotto@gmail.com>
2024-11-04 08:25:57 +0100

    added old in update and set prevent value in case of none

--- resources/views/plants/edit.blade.php
+++ resources/views/plants/edit.blade.php
@@ -11,27 +11,27 @@
 
     <div class="mb-3">
       <label for="name" class="form-label">nome pianta:</label>
-      <input type="text" class="form-control" id="plant-name" name="name" value="{{ $plant->name}}">
+      <input type="text" class="form-control" id="plant-name" name="name" value="{{ old('name'), $plant->name}}">
     </div>
     <div class="mb-3">
         <label for="species" class="form-label">specie pianta:</label>
-        <input type="text" class="form-control" id="plant-species" name="species" value="{{ $plant->species}}">
+        <input type="text" class="form-control" id="plant-species" name="species" value="{{ old('species') $plant->species}}">
       </div>
       <div class="mb-3">
         <label for="habitat" class="form-label">habitat pianta:</label>
-        <input type="text" class="form-control" id="plant-habitat" name="habitat" value="{{ $plant->habitat}}">
+        <input type="text" class="form-control" id="plant-habitat" name="habitat" value="{{ old('habitat') $plant->habitat}}">
       </div>
       <div class="mb-3">
         <label for="exposure" class="form-label">esposizione pianta:</label>
-        <input type="text" class="form-control" id="plant-exposure" name="exposure" value="{{ $plant->exposure}}">
+        <input type="text" class="form-control" id="plant-exposure" name="exposure" value="{{ old('exposure') $plant->exposure}}">
       </div>
       <div class="mb-3">
         <label for="description" class="form-label">descrizione pianta:</label>
-        <input type="text" class="form-control" id="plant-description" name="description" value="{{ $plant->description}}">
+        <input type="text" class="form-control" id="plant-description" name="description" value="{{ old('description') $plant->description}}">
       </div>
       <div class="mb-3">
         <label for="image" class="form-label">immagine pianta:</label>
-        <input type="text" class="form-control" id="plant-image" name="image" value="{{ $plant->image}}">
+        <input type="text" class="form-control" id="plant-image" name="image" value="{{ old('image') $plant->image}}">
       </div>
       <div class="buttons">
           <button type="submit" class="btn btn-primary">Invia</button>
