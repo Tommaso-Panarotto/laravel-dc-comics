01834f81f2054ab6ed41f2ba6e93738036d075a1
Tommaso-Panarotto <tommaso.panarotto@gmail.com>
2024-11-04 08:39:45 +0100

    error on single element

--- resources/views/plants/create.blade.php
+++ resources/views/plants/create.blade.php
@@ -6,44 +6,62 @@
 <div class="container text-center">
   <h1 class="mt-5">Aggiungi una nuova pianta</h1>
 
-  @if ($errors->any())
-  <div class="alert alert-info">
-      <ul>
-          @foreach ($errors->all() as $error)
-          <li>
-              {{$error}}
-            </li>
-            @endforeach
-        </ul>
-    </div>
-    @endif
-
   <form class="mt-5" method="POST" action="{{route("plant.store")}}">
     @csrf
 
     <div class="mb-3">
       <label for="name" class="form-label">nome pianta:</label>
       <input type="text" class="form-control" id="plant-name" name="name" value="{{ old('name')}}">
+      @error('name')
+    <div class="alert alert-warning">
+      {{$message}}
+      </div>
+      @enderror
     </div>
     <div class="mb-3">
         <label for="species" class="form-label">specie pianta:</label>
         <input type="text" class="form-control" id="plant-species" name="species" value="{{ old('species')}}">
+        @error('species')
+    <div class="alert alert-warning">
+      {{$message}}
+      </div>
+      @enderror
       </div>
       <div class="mb-3">
         <label for="habitat" class="form-label">habitat pianta:</label>
         <input type="text" class="form-control" id="plant-habitat" name="habitat" value="{{ old('habitat')}}">
+        @error('habitat')
+    <div class="alert alert-warning">
+      {{$message}}
+      </div>
+      @enderror
       </div>
       <div class="mb-3">
         <label for="exposure" class="form-label">esposizione pianta:</label>
         <input type="text" class="form-control" id="plant-exposure" name="exposure" value="{{ old('exposure')}}">
+        @error('exposure')
+    <div class="alert alert-warning">
+      {{$message}}
+      </div>
+      @enderror
       </div>
       <div class="mb-3">
         <label for="description" class="form-label">descrizione pianta:</label>
         <input type="text" class="form-control" id="plant-description" name="description" value="{{ old('description')}}">
+        @error('description')
+    <div class="alert alert-warning">
+      {{$message}}
+      </div>
+      @enderror
       </div>
       <div class="mb-3">
         <label for="image" class="form-label">immagine pianta:</label>
         <input type="text" class="form-control" id="plant-image" name="image" value="{{ old('image')}}">
+        @error('image')
+    <div class="alert alert-warning">
+      {{$message}}
+      </div>
+      @enderror
       </div>
       <div class="buttons">
           <button type="submit" class="btn btn-primary">Invia</button>
--- resources/views/plants/edit.blade.php
+++ resources/views/plants/edit.blade.php
@@ -11,27 +11,57 @@
 
     <div class="mb-3">
       <label for="name" class="form-label">nome pianta:</label>
-      <input type="text" class="form-control" id="plant-name" name="name" value="{{ old('name'), $plant->name}}">
+      <input type="text" class="form-control" id="plant-name" name="name" value="{{ old('name', $plant->name) }}">
+      @error('name')
+    <div class="alert alert-warning">
+      {{$message}}
+      </div>
+      @enderror
     </div>
     <div class="mb-3">
         <label for="species" class="form-label">specie pianta:</label>
-        <input type="text" class="form-control" id="plant-species" name="species" value="{{ old('species') $plant->species}}">
+        <input type="text" class="form-control" id="plant-species" name="species" value="{{ old('species', $plant->species) }}">
+        @error('species')
+    <div class="alert alert-warning">
+      {{$message}}
+      </div>
+      @enderror
       </div>
       <div class="mb-3">
         <label for="habitat" class="form-label">habitat pianta:</label>
-        <input type="text" class="form-control" id="plant-habitat" name="habitat" value="{{ old('habitat') $plant->habitat}}">
+        <input type="text" class="form-control" id="plant-habitat" name="habitat" value="{{ old('habitat', $plant->habitat) }}">
+        @error('habitat')
+    <div class="alert alert-warning">
+      {{$message}}
+      </div>
+      @enderror
       </div>
       <div class="mb-3">
         <label for="exposure" class="form-label">esposizione pianta:</label>
-        <input type="text" class="form-control" id="plant-exposure" name="exposure" value="{{ old('exposure') $plant->exposure}}">
+        <input type="text" class="form-control" id="plant-exposure" name="exposure" value="{{ old('exposure', $plant->exposure) }}">
+        @error('exposure')
+    <div class="alert alert-warning">
+      {{$message}}
+      </div>
+      @enderror
       </div>
       <div class="mb-3">
         <label for="description" class="form-label">descrizione pianta:</label>
-        <input type="text" class="form-control" id="plant-description" name="description" value="{{ old('description') $plant->description}}">
+        <input type="text" class="form-control" id="plant-description" name="description" value="{{ old('description', $plant->description) }}">
+        @error('description')
+    <div class="alert alert-warning">
+      {{$message}}
+      </div>
+      @enderror
       </div>
       <div class="mb-3">
         <label for="image" class="form-label">immagine pianta:</label>
-        <input type="text" class="form-control" id="plant-image" name="image" value="{{ old('image') $plant->image}}">
+        <input type="text" class="form-control" id="plant-image" name="image" value="{{ old('image', $plant->image) }}">
+        @error('image')
+    <div class="alert alert-warning">
+      {{$message}}
+      </div>
+      @enderror
       </div>
       <div class="buttons">
           <button type="submit" class="btn btn-primary">Invia</button>
